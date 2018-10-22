444f61aac942d7a91447f4252c02963a13171594
Jens Møller <jens@jensz12.com>
2018-10-22 21:55:00 +0200

    Added the website

---
+++ cat.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Jens er dejlig</title>
+
+</head>
+
+<body>
+<p>Jens er en svensk magikarp</p>
+<h4> cxm dcmdcxd,cm,cxm,x,xc,xc,x,xc,xc,.xcccxc,slzkmlksdklkskksssdzmkllklklsldx </h4>
+<p>Jeg er en trold</p>
+
+<h1> JENS ER DEJLIG </h1>
+<h2> JENS ER DEJLIG </h2>
+<h3> JENS ER DEJLIG 1</h3>
+<h4> JENS ER DEJLIG </h4>
+<img src="/derpemote2.png" alt="Skal du spille smart?" >
+<p> Hva så makker </p>
+<h1 align="right"> <a href="http://erduitvivl.dk" > Har du haft en dårlig dag? </a> </h1>
+
+<img src="/JensDanse.gif" alt="Skal du spille smart?">
+
+
+  <h2> Jens </h2>
+  <p> Jens er en basic jyde som, ikke kan finde ud af at snakke dansk. Hver gang han prøver begynder han bare at synge. </p>
+  <p> Donate til HjælpEnJyde lige her </p>
+  
+  <h1 align="right"> HER ER JENS' LIVSHISTORIE </h1>
+  <p align="right"> Der var en gang en Jens. Han elskede sin kæreste Catrine, så han ville give hende at i hele verdenen. SLUT </p>
+  
+  
+</body>
+ 
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,94 @@
+﻿<!--
+   _____      _                  _                    ______  
+  / ____|    | |      _         | |                  / /___ \ 
+ | |     __ _| |_   _| |_       | | ___ _ __  ___   / /  __) |
+ | |    / _` | __| |_   _|  _   | |/ _ \ '_ \/ __| < <  |__ < 
+ | |___| (_| | |_    |_|   | |__| |  __/ | | \__ \  \ \ ___) |
+  \_____\__,_|\__|          \____/ \___|_| |_|___/   \_\____/ 
+                                                              
+                                                              
+-->
+<!DOCTYPE html>
+<html lang="da-DK">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Cat & Jens ❤️</title>
+<link rel="icon" href="https://catogjens.info/emoji_u2665.png">
+<meta name="description" content="❤️">
+<meta name="theme-color" content="#A81010">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@jensz12">
+<meta name="twitter:creator" content="@jensz12">
+<meta name="twitter:title" content="Cat & Jens ❤️">
+<meta name="twitter:description" content="❤️">
+<meta name="twitter:image:src" content="https://catogjens.info/20180427_164231.jpg">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+<link rel="manifest" href="/manifest.json">
+<link rel="image_src" href="https://catogjens.info/20180427_164231.jpg">
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-POYwD7xcktv3gUeZO5s/9nUbRJG/WOmV6jfEGikMJu77LGYO8Rfs2X7URG822aum" crossorigin="anonymous">
+<style>
+body {
+	background-image: url(https://catogjens.info/20180427_164231.jpg);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center center;
+	background-attachment: fixed;
+	height: 100%;
+	font-family: 'Roboto', sans-serif;
+	padding-top: 70px;
+}
+h1 {
+  	text-align: center;
+	margin-top:0px;
+}
+.fa-heart {
+	color: red;
+}
+</style>
+</head>
+<body>
+	<main role="main" id="content" class="container">
+		<div class="row">
+			<article class="col-lg-12 col-md-12">
+				<div class="jumbotron">
+					<h1> <i class="fas fa-heart" data-toggle="tooltip" data-placement="top" title="25/07/2017" data-toggle="modal" data-target="#exampleModal"></i></h1>
+					<h1 id="countdown"></h1>
+				</div>
+				<div class="jumbotron">
+					<div class="embed-responsive embed-responsive-16by9">
+  						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Arj8WZJMPIo?showinfo=0&color=white&theme=light&fs=1" allowfullscreen></iframe>
+					</div>
+				</div>
+			</article>
+		</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"crossorigin="anonymous"></script>
+<!--<script src="https://static.jensz12.com/script/snowstorm.js"></script>-->
+<script>
+	var countDownDate = new Date("Oct 26, 2018 17:45:00").getTime();
+	var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = days + " dage " + hours + " timer "
+    + minutes + " minutter " + seconds + " sekunder "; 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "❤️";
+    }
+}, 1000);
+</script>
+<script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
+</body>
+</html>
