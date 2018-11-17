28c4aa5725699c9e53cc0a696d452cdbc25d4c2d
Jens Møller <jens@jensz12.com>
2018-11-17 21:34:04 +0100

    Fugtige migmiger

---
+++ migmig.php
@@ -0,0 +1,67 @@
+﻿<!--
+
+  ______ _    _  _____ _______ _____ _____ ______   __  __ _____ _____ __  __ _____ _____ ______ _____  
+ |  ____| |  | |/ ____|__   __|_   _/ ____|  ____| |  \/  |_   _/ ____|  \/  |_   _/ ____|  ____|  __ \ 
+ | |__  | |  | | |  __   | |    | || |  __| |__    | \  / | | || |  __| \  / | | || |  __| |__  | |__) |
+ |  __| | |  | | | |_ |  | |    | || | |_ |  __|   | |\/| | | || | |_ | |\/| | | || | |_ |  __| |  _  / 
+ | |    | |__| | |__| |  | |   _| || |__| | |____  | |  | |_| || |__| | |  | |_| || |__| | |____| | \ \ 
+ |_|     \____/ \_____|  |_|  |_____\_____|______| |_|  |_|_____\_____|_|  |_|_____\_____|______|_|  \_\
+                                                                                                        
+                                                                                                        
+                                                           
+-->
+<!DOCTYPE html>
+<html lang="da-DK">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Fugtige MigMiger</title>
+<link rel="icon" href="https://catogjens.info/emoji_u2665.png">
+<meta name="description" content="Fugtige MigMiger fra fiskens meme fabrik">
+<meta name="theme-color" content="#A81010">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@jensz12">
+<meta name="twitter:creator" content="@jensz12">
+<meta name="twitter:title" content="Fugtige MigMiger">
+<meta name="twitter:description" content="Fugtige MigMiger fra fiskens meme fabrik">
+<meta name="twitter:image:src" content="https://catogjens.info/20180427_164231.jpg">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+<link rel="manifest" href="/manifest.json">
+<link rel="image_src" href="https://catogjens.info/20180427_164231.jpg">
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-POYwD7xcktv3gUeZO5s/9nUbRJG/WOmV6jfEGikMJu77LGYO8Rfs2X7URG822aum" crossorigin="anonymous">
+<style>
+body {
+	background-image: url(https://catogjens.info/20180427_164231.jpg);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center center;
+	background-attachment: fixed;
+	font-family: 'Roboto', sans-serif;
+	padding-top: 70px;
+}
+h1 {
+  	text-align: center;
+	margin-top:0px;
+}
+</style>
+</head>
+<body>
+	<main role="main" id="content" class="container">
+		<div class="row">
+			<article class="col-lg-12 col-md-12">
+				<div class="jumbotron">
+				<img src="/pepe.png" class="img-fluid" alt="Pepe">
+				</div>
+				<div class="jumbotron">
+				<img src="gldelig_halloween.png" class="img-fluid" alt="Haloween">
+				</div>
+			</article>
+		</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"crossorigin="anonymous"></script>
+<script src="https://static.jensz12.com/script/snowstorm.js"></script>
+</body>
+</html>
